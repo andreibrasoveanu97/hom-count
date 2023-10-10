eb485c54d931aaef201b1dccd27499521db8223c
andreibrasoveanu97 <andrei.brasoveanu1997@gmail.com>
2023-10-10 19:47:12 +0200

    Add: setup for ZINC CWN experiments

--- Scripts/GlobalFeatures_Individual/ZINC.sh
+++ Scripts/GlobalFeatures_Individual/ZINC.sh
@@ -1,9 +1,17 @@
-declare -a feature=("WIENER" "HOSOYA" "INDEPENDENCE" "SECOND_EIGEN" "CIRCUIT_RANK" "SPECTRAL_RADIUS" "TRUEHOSOYA" "ZAGREB_M1" "ZAGREB_M2")
+declare -a feature=("WIENER" "HOSOYA" "INDEPENDENCE" "SECOND_EIGEN" "CIRCUIT_RANK" "SPECTRAL_RADIUS" "TRUEHOSOYA" "ZAGREB_M1" "ZAGREB_M2" "ALL")
 
-# ZINC
+# ZINC-trafo
 for i in "${!feature[@]}"
 do
   echo "${feature[i]}"
     # GIN with features
-    python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gin_with_features_individual.yaml" -dataset "ZINC" --candidates 1  --repeats 10 --graph_feat "Counts/GlobalFeatures/ZINC_${feature[i]}_global.json"
+    python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gin_trafo.yaml" -dataset "ZINC" --candidates 1  --repeats 10 --graph_feat "Counts/GlobalFeatures/ZINC_${feature[i]}_global.json"
     done
+
+# ZINC-gin
+#for i in "${!feature[@]}"
+#do
+#  echo "${feature[i]}"
+#    # GIN with features
+#    python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gin_with_features_individual.yaml" -dataset "ZINC" --candidates 1  --repeats 10 --graph_feat "Counts/GlobalFeatures/ZINC_${feature[i]}_global.json"
+#    done
